bd076976eb4ce6962f4dbc89ff2529352b353382
Ajay Lakshminarayanarao <alkutnikar@gmail.com>
2014-11-29 00:51:53 -0500

    With Actual Trends

--- TrendsData/trends.py
+++ TrendsData/trends.py
@@ -4,5 +4,29 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 mpl.rc('figure', figsize=(8, 7))
 
-data = pd.read_csv('aapl.csv', index_col='Date', parse_dates=True)
+data = pd.read_csv('a.csv', index_col='Date', parse_dates=True)
 data.head()
+
+data.plot(label='actual search');
+search_trend = pd.rolling_mean(data['Search'], 5)
+search_trend.plot(label='search_trend')
+plt.legend()
+plt.show()
+
+
+data['order'] = 0
+data['order'][data['Search'] > search_trend] = -1 # Short if search volume goes up relative to mavg.
+data['order'][data['Search'] < search_trend] = 1 # Long if search volume goes down relative to mavg.
+print data
+
+data['ret_search'] = data['Search'].pct_change()
+print data
+
+data['ret_search'] = data['ret_search'].shift(-1)
+
+data['ret_aapl'] = data.order * data.ret_search
+print data
+
+(1 + data.ret_aapl).cumprod().plot();
+plt.ylabel('Portfolio value')
+plt.show()
--- getStock.py
+++ getStock.py
@@ -7,12 +7,29 @@ import pandas.io.data
 from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-mpl.rc('figure',figsize=(8,7))
+import pylab
+from pylab import *
+import getMAvg
+from getMAvg import *
 
 for comp in compDict.keys():
+    mpl.rc('figure',figsize=(13,13))
     tempStock = pd.io.data.get_data_yahoo(compDict[comp], 
                                  start=datetime.datetime(2009, 1, 1), 
                                  end=datetime.datetime(2014, 12, 2))
+    close_px = tempStock['Adj Close']
+    mavg = pd.rolling_mean(close_px, 10)
+    exavg = pd.ewma(close_px, 10)
+    #mavg[-10:]
+    #exavg[-10:]
+    close_px.plot(label=comp)
+    mavg.plot(label='mavg') 
+    exavg.plot(label='exavg')
+    #plt.figure()
+    #plt.hold(False)
+    plt.legend()
+    #pylab.savefig('MvAvgPlots/'+comp+'.png')
+    #plt.show();
     tempStock.head()
     comp = comp.replace(' ','')
     
